6de2d39ef4e4dcb9a404e487d18b9b76ad0505f8
Dipesh Rai <raidipesh78@gmail.com>
2021-09-25 22:05:28 +0545

    Updates

---
+++ static/js/main.fe901f8a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdipeshrai123.github.io"]=this["webpackJsonpdipeshrai123.github.io"]||[]).push([[0],{4:function(e,i,t){"use strict";t.r(i);t(0);var o=t(2),s=t.n(o),n=t(3);s.a.render(Object(n.jsx)("div",{children:"Welcome to my page"}),document.getElementById("root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.fe901f8a.chunk.js.map
